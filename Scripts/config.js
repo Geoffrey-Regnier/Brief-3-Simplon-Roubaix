add5ea2a7ef88ba9b38a70828f6d9113dbd5d760
Geo <131854488+Qui-Gon-Jo@users.noreply.github.com>
2023-10-02 12:14:32 +0200

    Utilisateurs récupérés et implantés

--- Scripts/config.js
+++ Scripts/config.js
@@ -1,38 +1,48 @@
-fetch ('https://reqres.in/api/users?page=2',{method:'get'})
-.then( response => {
+let main = document.getElementById("root");
+let etiquettesHTML = "";
+let styleEtiquette = document.querySelectorAll("main div")
+
+function recupererEtiquette () {
+
+    fetch('https://reqres.in/api/users?page=1', { method: 'get' })
+  .then(response => {
     return response.json();
-})
-.then (data => {
-    let i = 0
-    let user = data.data
-    let main = document.getElementById("main")
+  })
+  .then(data => {
+    let user = data.data;
 
-    for (i ; i < user.length ; i++) {
-
-        creerEtiquette()
-        function creerEtiquette() {
-            userID = user[i].id 
-            userEmail = user[i].email 
-            userFirstName = user[i].first_name 
-            userLastName = user[i].last_name
-            userAvatar = user[i].avatar
-        
-            etiquette = `
-            <div>
-                <div><img src="${userAvatar}" alt=""></div>
-                <p>${userID}</p>
-                <p>${userFirstName}</p>
-                <p>${userLastName}</p>
-                <p>${userEmail}</p>
-            </div>
-             `;
-        
-            main.innerHTML = etiquette
-        }
+    for (let i = 0; i < user.length; i++) {
+      etiquettesHTML += creerEtiquette(user[i]);
     }
 
-   
-})
+    fetch('https://reqres.in/api/users?page=2', { method: 'get' })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        let user = data.data;
 
+        for (let i = 0; i < user.length; i++) {
+          etiquettesHTML += creerEtiquette(user[i]);
+        }
 
+        main.innerHTML = etiquettesHTML;
+      });
+  });
 
+    function creerEtiquette(user) {
+    let userFirstName = user.first_name;
+    let userLastName = user.last_name;
+    let userAvatar = user.avatar;
+
+    let etiquette = `
+        <div>
+        <div><img src="${userAvatar}" alt="Photo de l'utilisateur"></div>
+        <p>${userFirstName}</p>
+        <p>${userLastName}</p>
+        </div>
+    `;
+
+    return etiquette;
+    }
+}
--- readme..txt
+++ readme..txt
@@ -3,3 +3,8 @@ Votre entreprise fournit des solutions logicielles aux entreprises pour gérer e
 Vous avez récemment obtenu un contrat avec une nouvelle entreprise cliente, "TechCorp Inc.", qui recherche une interface simple et conviviale pour afficher et gérer les profils de ses employés. 
 Le projet "UserExplorer" consiste à créer cette interface en utilisant les technologies web de base (HTML, CSS et JavaScript) pour consommer une API REST fournissant des données utilisateur. 
 Cette interface doit permettre à TechCorp Inc. de visualiser les profils des utilisateurs et, en cliquant sur l'un d'entre eux, obtenir tous les renseignements sur cet utilisateur.
+
+une boucle qui ira chercher les infos et les incrémentera
+
+
+Je veux créer une étiquette pour chaque users
